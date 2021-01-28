82c09e9153edfd57b009fd887d936dc1abab3a42
Will Gash <wttgash489@gmail.com>
2021-01-28 21:40:41 +0000

    Added a package.json file to configure all dev dependencies for the Todo app

---
+++ Todo/Models/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo.Models
+{
+    public class TodoItem
+    {
+        public bool Status { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ Todo/Pages/ToDoListComponent.razor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Todo.Models;
+
+namespace Todo.Pages
+{
+    public partial class ToDoListComponent
+    {
+
+        public IList<TodoItem> TodoItems { get; } = new List<TodoItem> { new TodoItem { Name = "Add Css" }, new TodoItem { Name = "Add API" }, new TodoItem { Name = "Get some grub" } };
+
+        public string NewTodoItemName { get; set; }
+
+        public void AddTodoItemToList()
+        {
+            TodoItems.Add(new TodoItem() { Name = NewTodoItemName });
+            NewTodoItemName = string.Empty;
+        }
+    }
+}
