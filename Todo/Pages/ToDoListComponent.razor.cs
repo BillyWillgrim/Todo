8fadb7b4e4b93070350ed2381637fec239e648c4
Will Gash <wttgash489@gmail.com>
2021-01-29 15:32:45 +0000

    added validation message

--- Todo/Pages/ToDoListComponent.razor.cs
+++ Todo/Pages/ToDoListComponent.razor.cs
@@ -11,7 +11,9 @@ namespace Todo.Pages
 
         public IList<TodoItem> TodoItems { get; } = new List<TodoItem> { new TodoItem { Name = "Add Css" }, new TodoItem { Name = "Add API" }, new TodoItem { Name = "Get some grub" } };
 
-        public string NewTodoItemName { get; set; }
+        public string NewTodoItemName { get; set; } = string.Empty;
+
+        public bool IsInputEmpty => NewTodoItemName == string.Empty;
 
         public void AddTodoItemToList()
         {
