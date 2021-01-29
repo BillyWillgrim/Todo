0fc95c9f380e2ec14b2da7a54b71239a16b44b8c
Will Gash <wttgash489@gmail.com>
2021-01-29 16:32:13 +0000

    Added basic editing of todo items:
    Edit/Save

---
+++ Todo/Pages/TodoItemComponent.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Todo.Models;
+
+namespace Todo.Pages
+{
+    public partial class TodoItemComponent
+    {
+        [Parameter] public TodoItem TodoItem { get; set; }
+
+
+        public string NewName { get; set; }
+
+        public bool Editing { get; set; }
+
+        public void Edit()
+        {
+            NewName = TodoItem.Name;
+            Editing = true;
+        }
+        public void Save()
+        {
+            TodoItem.Name = NewName;
+            Editing = false;
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine($"Deleting {TodoItem.Name}");
+        }
+    }
+}
